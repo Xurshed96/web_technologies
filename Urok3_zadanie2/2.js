b58ba3cbdb5b9a184a465340ba64296fe9508917
xurshed_2016@mail.ru <xurshed_2016@mail.ru>
2023-07-10 12:12:36 +0300

    solution

---
+++ Urok3_zadanie2/2.js
@@ -0,0 +1,10 @@
+// Define the greeting function
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+// Prompt the user for a name
+var userName = prompt("Enter your name:");
+
+// Call the greeting function with the user's name
+greet(userName);
